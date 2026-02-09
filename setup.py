1fce13967a96b430cbde5dae43626e5c4cb499c2
ner20001941-prog'sProjects <ner20001941@gmail.com>
2026-02-09 17:42:10 +0300

    Update for deployment

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="telegram-bot-fix",
+    name="telegram-bot",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'python-telegram-bot==13.15',
+        'urllib3==1.26.18',
+        'six==1.16.0',
     ],
-    # Добавляем заглушку для imghdr
-    py_modules=['imghdr'],
 )
